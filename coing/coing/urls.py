729b65567059ed06ba14c1052064e59a305b8759
Vincent Poirot <vpoirot@k1r3p3.42mulhouse.fr>
2024-08-14 13:30:34 +0200

    correc django + add modal

--- coing/coing/urls.py
+++ coing/coing/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from game import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('game.urls')),
+    path('', views.index),
 ]
