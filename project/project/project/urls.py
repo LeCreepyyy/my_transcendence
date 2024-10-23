8816e49d26c36cff36c4d23c31b89aa0bfe5593e
Vincent Poirot <vpoirot@k2r3p8.42mulhouse.fr>
2024-10-23 13:28:59 +0200

    add profile picture

--- project/project/project/urls.py
+++ project/project/project/urls.py
@@ -8,6 +8,9 @@ from django.contrib.auth import views as auth_views
 
 from app.views import register, home, TwoFactorLoginWithJWT
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     
@@ -24,3 +27,6 @@ urlpatterns = [
 
     path('home/', home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
